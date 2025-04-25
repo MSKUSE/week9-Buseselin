22c5be0785d19d98319c3ecbd180dd3ee7c43d8a
Buseselin <buseselinozmen@posta.mu.edu.tr>
2025-04-25 11:57:02 +0300

    week9

---
+++ src/Drawaable.java
@@ -0,0 +1,6 @@
+public interface Drawaable {
+
+    public void draw ();
+
+
+}
--- src/Shape.java
+++ src/Shape.java
@@ -1,28 +1,19 @@
-public class Shape {
+public abstract class Shape {
 
-    private String color = "white";
+    private String color="white";
+
 
     public Shape(){
 
     }
+
+
     public Shape(String color) {
         this.color = color;
     }
 
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double area(){
-        return 0;
-    }
-    public double perimeter(){
-        return 0;
-    }
+    public abstract double perimeter();
+    public abstract double area();
 
 
     @Override
